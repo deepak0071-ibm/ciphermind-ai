4b93d3a16db118d864dac0a29b50763a42426030
DeepakKumar <dkumar1inida@gmail.com>
2026-02-17 13:29:59 -0800

    Fix encryption engine key initialization

--- encryption.py
+++ encryption.py
@@ -1,16 +1,32 @@
 from cryptography.fernet import Fernet
+import os
+
 
 class EncryptionEngine:
 
     def __init__(self, key=None):
-        if key is None:
-            key = Fernet.generate_key()
 
-        self.key = key
-        self.cipher = Fernet(self.key)
+        if key is None:
+
+            key = os.environ.get("CIPHERMIND_KEY")
+
+            if key is None:
+
+                key = Fernet.generate_key().decode()
+
+                print("Generated new encryption key")
+
+        if isinstance(key, str):
+            key = key.encode()
+
+        self.cipher = Fernet(key)
+
 
     def encrypt(self, text):
+
         return self.cipher.encrypt(text.encode()).decode()
 
+
     def decrypt(self, encrypted_text):
+
         return self.cipher.decrypt(encrypted_text.encode()).decode()
