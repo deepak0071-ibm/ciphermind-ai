c4c1c582cbf95b51e35e3b7efc9533d4d320c4ed
DeepakKumar <dkumar1inida@gmail.com>
2026-02-17 07:25:45 -0800

    Fix EncryptionEngine key initialization

--- encryption.py
+++ encryption.py
@@ -1,16 +1,16 @@
 from cryptography.fernet import Fernet
-import base64
-import hashlib
 
 class EncryptionEngine:
 
-    def __init__(self, key):
-        key_bytes = hashlib.sha256(key.encode()).digest()
-        self.key = base64.urlsafe_b64encode(key_bytes)
+    def __init__(self, key=None):
+        if key is None:
+            key = Fernet.generate_key()
+
+        self.key = key
         self.cipher = Fernet(self.key)
 
     def encrypt(self, text):
         return self.cipher.encrypt(text.encode()).decode()
 
-    def decrypt(self, encrypted):
-        return self.cipher.decrypt(encrypted.encode()).decode()
+    def decrypt(self, encrypted_text):
+        return self.cipher.decrypt(encrypted_text.encode()).decode()
